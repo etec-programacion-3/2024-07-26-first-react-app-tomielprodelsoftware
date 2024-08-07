5134f86995dc256235466d55bd9ff3ccbe440161
tomi fuentes <107146799+tomielprodelsoftware@users.noreply.github.com>
2024-08-07 13:06:25 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import HolaMundo from "./components/HolaMundo";
+
+function App() {
+  return (<div><HolaMundo /> 
+  <p>Programacion 2</p></div>)
+}
+export default App
---
+++ src/components/HolaMundo.jsx
@@ -0,0 +1,4 @@
+function HolaMundo() {
+    return (<h1>lorem ipsum</h1>)
+  }
+  export default HolaMundo;
